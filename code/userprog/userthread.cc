eee135dcdfe057ba2daf4f69f95751b703629483
Ludovic Delavois <ldelavois@emi.u-bordeaux1.fr>
2018-10-17 17:10:47 +0200

    ajout du userthread.cc

---
+++ code/userprog/userthread.cc
@@ -0,0 +1,25 @@
+#include "copyright.h"
+#include "system.h"
+#include "console.h"
+#include "synchconsole.h"
+#include "addrspace.h"
+#include "synch.h"
+
+
+void ThreadExit()
+
+{
+if 1
+{
+int ThreadCreate(void f(void *arg), void *arg);
+
+void ThreadExit(void);
+
+}
+
+else return -1;
+
+
+
+
+}
